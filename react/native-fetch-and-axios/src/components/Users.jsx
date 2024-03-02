03949c50a74724ae80bb543b55bda0cfdc76c0c8
sevgiguclu <guclusevgi97@gmail.com>
2024-03-02 18:02:03 +0300

    applied axios

--- react/native-fetch-and-axios/src/components/Users.jsx
+++ react/native-fetch-and-axios/src/components/Users.jsx
@@ -1,22 +1,33 @@
 import {useEffect,useState} from 'react'
 
+import axios from 'axios'
+
 function Users() {
   const [userList,setUserList] = useState([]);
   const [isLoading,setIsLoading] = useState(true);
+  const [userPosts,setUserPosts] = useState([]);
   
   useEffect(() => {
+    // native fetch 
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(data => setUserList(data))
       .catch(e => console.log(e))
       .finally(() => setIsLoading(false))
+
+    // axios fetch
+    axios('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => setUserPosts(res.data))
+    .catch(e => console.log(e))
+    .finally(() => setIsLoading(false))
+
   },[]);
 
   return (
    <>
     <h2>User Fetch</h2>
     <p>{isLoading && <span>Loading...</span>}</p>
-    <ul>
+    <ul className='inlineList'>
       {
         userList.map((item) => 
           <li key={item.id}>{item.name}</li>
@@ -24,7 +35,13 @@ function Users() {
         )
       }
     </ul>
-    
+    <ul className='inlineList'>
+      {
+        userPosts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))
+      }
+    </ul>
    </>
     
   )
